170d51eef1e8a5c056a2e71599436725ccde3fcf
tarun <tarun.singh1120@gmail.com>
2024-03-06 23:18:24 +0530

    server side url

--- src/app/apiutility.ts
+++ src/app/apiutility.ts
@@ -1,4 +1,2 @@
-// export const apiUrl = "http://44.210.130.132:8080";
 // export const apiUrl = "http://localhost:8080";
-export const apiUrl =
-  "https://store.ashyground-d1c02638.eastus.azurecontainerapps.io";
+export const apiUrl = "https://store-1709745297398.azurewebsites.net";
