b7985f38048cfeac2572daa154676cc7526f56bb
tarun <tarun.singh1120@gmail.com>
2024-03-06 11:12:02 +0530

    azure spring

--- src/app/apiutility.ts
+++ src/app/apiutility.ts
@@ -1,3 +1,4 @@
 // export const apiUrl = "http://44.210.130.132:8080";
 // export const apiUrl = "http://localhost:8080";
-export const apiUrl = "https://springboot-azure.onrender.com";
+export const apiUrl =
+  "https://store.ashyground-d1c02638.eastus.azurecontainerapps.io";
