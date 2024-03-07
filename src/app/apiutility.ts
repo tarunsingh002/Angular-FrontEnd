5b7f647296a1bd7ccb48b1d8b9232ed015553bde
tarun <tarun.singh1120@gmail.com>
2024-03-07 18:00:22 +0530

    azure spring apps

--- src/app/apiutility.ts
+++ src/app/apiutility.ts
@@ -1,2 +1,4 @@
 // export const apiUrl = "http://localhost:8080";
-export const apiUrl = "https://store-1709745297398.azurewebsites.net";
+// export const apiUrl = "https://store-1709745297398.azurewebsites.net";
+export const apiUrl =
+  "https://store.kindsmoke-53836ce6.eastus.azurecontainerapps.io/";
